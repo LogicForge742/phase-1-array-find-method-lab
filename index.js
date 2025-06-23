4c8a829c5cf64cdb54b67ccfe46d2e9876ec9622
Milton Ngeno <ngenomilton10@gmail.com>
2025-06-24 01:02:32 +0300

    adds using the array find method

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(records){
+  // Use find() to locate the first object with result "W"
+  const winRecord = records.find(record => record.result === "W");
+
+  // If a winning record is found, return its year; otherwise, return undefined
+  return winRecord ? winRecord.year : undefined;
+}
+
